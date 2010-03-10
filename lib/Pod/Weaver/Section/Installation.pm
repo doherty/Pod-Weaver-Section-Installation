9815fcdef26324e0db741d69328dcccda09134ba
Marcel Gruenauer <hanekomu@gmail.com>
2010-03-10 14:09:43 +0100

    initial commit

---
+++ lib/Pod/Weaver/Section/Installation.pm
@@ -0,0 +1,37 @@
+package Pod::Weaver::Section::Installation;
+
+# ABSTRACT: add an INSTALLATION pod section
+use Moose;
+with 'Pod::Weaver::Role::Section';
+use namespace::autoclean;
+use Moose::Autobox;
+
+sub weave_section {
+    my ($self, $document, $input) = @_;
+    $document->children->push(
+        Pod::Elemental::Element::Nested->new(
+            {   command  => 'head1',
+                content  => 'INSTALLATION',
+                children => [
+                    Pod::Elemental::Element::Pod5::Ordinary->new(
+                        {   content =>
+'See perlmodinstall for information and options on installing Perl modules.'
+                        }
+                    ),
+                ],
+            }
+        ),
+    );
+}
+1;
+
+=head1 SYNOPSIS
+
+In C<weaver.ini>:
+
+    [Installation]
+
+=head1 OVERVIEW
+
+This section plugin will produce a hunk of Pod that describes how to install
+the distribution.
